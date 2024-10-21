cef5a9d91c81fa2262ff0b58d3f6f262799a1ebf
Rowan van Rooijen <rowan@rvr.dev>
2024-10-22 00:26:20 +0200

    Updated structure

---
+++ server-root/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MongoDriver } from '@mikro-orm/mongodb';
+
+@Module({
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'my-db-name.sqlite3',
+      driver: MongoDriver,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ server-root/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('W.A.F.F.L.E. API')
+      .setDescription('Api documentation for W.A.F.F.L.E.')
+      .setVersion('0.0.1-SNAPSHOT')
+      .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
